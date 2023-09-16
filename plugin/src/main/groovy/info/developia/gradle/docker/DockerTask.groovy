4a6fca0fc4a1e48c6e1372ddd8e998b4fdccb98e
manudevelopia <manugarciarodriguez@gmail.com>
2023-09-16 13:34:22 +0200

    move code to split methods

--- plugin/src/main/groovy/info/developia/gradle/docker/DockerTask.groovy
+++ plugin/src/main/groovy/info/developia/gradle/docker/DockerTask.groovy
@@ -2,17 +2,26 @@ package info.developia.gradle.docker
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.WorkResult
 
 class DockerTask extends DefaultTask {
 
     @TaskAction
     def run() {
+        copyApplicationDependencies()
+        copyApplicationJar()
+    }
+
+    private Iterable<File> copyApplicationDependencies() {
         project.configurations.runtimeClasspath.each { dependency ->
             project.copy {
                 from project.layout.buildDirectory.file(dependency.path)
                 into project.layout.buildDirectory.dir("${project.buildDir}/docker/slimjar")
             }
         }
+    }
+
+    private WorkResult copyApplicationJar() {
         project.copy {
             from "${project.buildDir}/libs/${project.name}.jar"
             into project.layout.buildDirectory.dir("${project.buildDir}/docker-slimjar")
